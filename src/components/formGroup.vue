10feef5150da780c8b8a8bcf78288f0457667693
Shestakov <shest_akow@rambler.ru>
2022-01-20 07:27:46 +0300

    main

---
+++ src/components/formGroup.vue
@@ -0,0 +1,17 @@
+<template>
+	<fieldset style="margin-top:10px;margin-bottom:10px;padding-left:10px;border-color:#EEE;border-radius:5px"  >
+		<legend>  {{header}}</legend>
+		<!--<v-card class="c-flex" style="margin-top:10px;margin-bottom:10px;padding-left:10px" >
+		<v-system-bar color="#EEE" style="font-weight: bold ;margin-bottom:10px">
+		  {{ header }} <v-spacer />
+			  </v-system-bar>
+			  </v-card>-->
+	</fieldset>
+</template>
+
+<script>
+	export default {
+		name: "formGroup",
+		props: ["header"],
+	};
+</script> 
