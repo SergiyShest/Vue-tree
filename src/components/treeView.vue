73391c4b371551ea8262c768f2ded6714b411e72
Shestakov <shest_akow@rambler.ru>
2022-01-31 10:05:52 +0300

    Fixsed problems

--- src/components/treeView.vue
+++ src/components/treeView.vue
@@ -1,10 +1,8 @@
 <template>
 <div>
-  
-
     <v-card class="mx-auto" max-width="500" max-height="1000">
        <vue-simple-context-menu
-      :elementId="'myContextMenu'"
+      :elementId="contextMenuId"
       :options="contextMenuItems"
       :ref="'contextMenu'"
       @option-clicked="contextMenuClicked"
@@ -73,6 +71,24 @@
 </template>
 
 <script>
+
+function generateguidID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+
 import { Drag, Drop, DropList } from "vue-easy-dnd";
 import VueSimpleContextMenu from "vue-simple-context-menu";
 export default {
@@ -91,6 +107,7 @@ export default {
     displaced: [],
     selection: [],
     openAll: false,
+    contextMenuId:generateguidID(),
 
     contextMenuItems: [
       {
@@ -113,18 +130,8 @@ export default {
       this.$refs.treeView.updateAll(this.openAll);
     },
     handleContextMenu(event, item) {
-      console.log(event);
-      console.log('pageX='+event.pageX);
-      console.log('clientX='+event.clientX);
-      console.log('screenX='+event.screenX); 
-      console.log('x='+event.x); 
-      console.log('offsetX='+event.offsetX);                  
-
       this.$refs.contextMenu.showMenu(event, item);
-     
- //     console.log('tree.offsetLeft'+tree.offsetLeft);   
- //     menu.style.left = event.pageX-menu.parentElement.offsetLeft - 2 + "px";
-//      console.log('menu.style.left='+menu.style.left);        
+
     },
     contextMenuClicked(event) {
       switch (event.option.slug) {
@@ -198,7 +205,7 @@ export default {
     },
   },
   mounted: function () {
-     var menu = document.getElementById('myContextMenu');
+     var menu = document.getElementById(this.contextMenuId);
       document.firstElementChild.appendChild(menu);       
   },
 };
